9349391032e580b21b78abc922e0eb1f9585d093
cortesantonio <antonio.cortes.sotelo@gmail.com>
2025-06-25 00:30:35 -0400

    se agrego estructura inicial de aplicacion

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+
+import Header from './components/Header'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Resume from './components/Resume'
+import "./App.css"
+
+export const paleta = {
+  primary: '#FF971D', // naranja 
+  secondary: '#F9F6F7', // blanco-crema
+  background: '#FFE8D6', // naranja claro-crema
+  text: '#111827', // gris oscuro
+  border: '#D1D5DB', // gris medio
+  accent: '#2563EB' // azul
+}
+
+
+function App() {
+
+  return (
+    <div className='flex flex-col items-center  min-h-scree' style={{ backgroundColor: paleta.background }}>
+      <div className="w-full fixed top-0 z-50" style={{ backgroundColor: paleta.background }}>
+        <Header />
+      </div>
+      <main className="xl:w-8/12 w-11/12">
+        <About />
+        <Projects />
+        <Resume />
+        <Contact />
+      </main>
+    </div>
+  )
+}
+
+export default App 
